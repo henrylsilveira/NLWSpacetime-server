d6a5019b4f35531411c5e873b8d8abfa54c3c066
Henry Leão <henrylsilveira@gmail.com>
2023-07-16 23:36:41 -0400

    16/07/2023

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,33 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
-// app.get('', () => {})
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
+app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
+
+app.register(cors, {
+  origin: true
+})
+app.register(jwt, {
+  secret: 'spacetime'
+})
 app.listen({
     port: 3333,
+    host: '0.0.0.0'
   })
   .then(() => {
     console.log('🚀 HTTP server running on http://localhost:3333')
